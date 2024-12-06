d36cde13413d3ca407ffea659e3f008078e360db
DarkNando29 <al19962009@gmail.com>
2024-12-06 14:15:44 -0600

    Update title on carrousel

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
       {/* Featured Destinations */}
       <section id="destinations" className="py-20 bg-gray-50">
         <div className="container mx-auto px-4">
-          <h2 className="text-4xl font-bold text-center mb-16">Featured Destinations</h2>
+          <h2 className="text-4xl font-bold text-center mb-16">Some of our journeys</h2>
           <div className="relative px-12">
             <Carousel
               opts={{
